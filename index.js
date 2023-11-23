968b48ffe84d20e89cc6e116672003018a53f1e0
Dana Maraqa <dana.maraqa@hotmail.com>
2023-11-23 22:06:17 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+
+function add(a,b) {
+
+    return a+b;
+    
+}
+add(n);
+
+
+
+function subtract(a,b) {
+    return a-b;
+}
+subtract(n);
+
+
+
+function multiply(a,b) {
+    return a*b;
+}
+multiply(n);
+
+
+
+function divide(a,b) {
+    return a/b;    
+}
+divide(n);
+
+
+function increment(n) {
+   
+    return ++n;   
+}
+increment(n);
+
+
+function decrement(n) {
+    return --n;    
+}
+decrement(n);
+
+
+
+function  makeInt(n){
+    return parseInt(n, 10) 
+    
+}
+makeInt(n);
+
+
+function  preserveDecimal(n) {
+
+    return parseFloat(n);
+    
+}
+
+preserveDecimal(n);
